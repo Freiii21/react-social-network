9f15f7b2a7a0b2f1d449d2c738d91c2938da9132
Freiii21 <mrfreiii@gmail.com>
2021-10-13 22:28:49 +0300

    #42 lesson_12

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 const App = () => {
     return (
-        <div className={"app-wrapper"}>
-            <header className={"header"}>
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/2560px-Playstation_logo_colour.svg.png"/>
-            </header>
-            <nav className={"nav"}>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className={"content"}>
-                <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg/700px-New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg"/>
-                </div>
-                <div>
-                    ava+desc
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>
-                            post1
-                        </div>
-                        <div>
-                            post2
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 
---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={"content"}>
+            <div>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg/700px-New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg"/>
+            </div>
+            <div>
+                ava+desc
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
