07371fe14db53543ece8d91ec5d8289ec5344173
Freiii21 <mrfreiii@gmail.com>
2021-10-20 10:18:35 +0300

    #42 lesson_23

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import s from './Profile.module.css'
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg/700px-New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg"/>
-            </div>
-            <div>
-                ava+desc
-            </div>
-            <div>
-                <MyPosts/>
-            </div>
+            <ProfileInfo/>
+            <MyPosts/>
         </div>
     )
 }
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './ProfileInfo.module.css'
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg/700px-New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg"/>
+            </div>
+            <div className={s.descriptionBlock}>
+                ava+desc
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo;
