b703e55d2044ec5d253c4341e6e363b001ca2703
Freiii21 <mrfreiii@gmail.com>
2021-10-26 09:45:19 +0300

    #42 lesson_24

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,11 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
 const MyPosts = () => {
+    let postsData = [
+        {id: 1, message: "hi, how are you?", likesCount: 12},
+        {id: 2, message: "It's my first post!", likesCount: 11},
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +20,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={'hi, how are you?'} likesCount={15}/>
-                <Post message={'It\'s my first post!'} likesCount={21}/>
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
             </div>
         </div>
     )
