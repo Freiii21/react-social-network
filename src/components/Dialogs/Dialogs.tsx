9b5cb2e8639680f1cebfbc511b730f6677e2c4a9
Freiii21 <mrfreiii@gmail.com>
2021-11-05 00:28:20 +0300

    #42 lesson_31

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -13,16 +13,28 @@ export const Dialogs = (props:DialogsPropsType) => {
 
     const dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} avatar={d.avatar}/>);
     const messagesElements = props.state.messages.map(m => <Message message={m.message} owner={m.owner} avatar={m.avatar}/>);
+    let textField = React.createRef<HTMLTextAreaElement>();
 
-    //UI
+    const sendMessage = () => {
+        let message = textField.current?.value;
+        alert(message);
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 {dialogsElements}
             </div>
             <div className={s.messages}>
-                {messagesElements}
+                <div>{messagesElements}</div>
+                <div className={s.answerField}>
+                    <div>
+                        <textarea className={s.inputField} placeholder={"Type a message"} ref={textField}></textarea>
+                    </div>
+                    <div><button className={s.sendMessage} onClick={sendMessage}>Send</button></div>
+                </div>
             </div>
+
         </div>
     )
 }
