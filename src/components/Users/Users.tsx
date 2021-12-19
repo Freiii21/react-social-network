75378e748df2732fbb412af9db53991809fbbcc9
Freiii21 <mrfreiii@gmail.com>
2021-12-19 19:37:16 +0300

    #42 lesson_57_2

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -16,7 +16,7 @@ export type UsersPropsType2 = {
 export const Users = (props:UsersPropsType2) => {
     const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
-    for (let i = 1; i <= pagesCount; i++){
+    for (let i = 1; i <= (pagesCount > 15 ? 15 : pagesCount); i++){
         pages.push(i);
     }
 
@@ -27,6 +27,7 @@ export const Users = (props:UsersPropsType2) => {
                     className={props.currentPage === p ? s.selectedPage : s.usual}
                     onClick={(e) => props.onPageChanged(p)}
                 >{p}</span>)}
+                {pagesCount > 15 ? <span> ... {pagesCount}</span> : null}
             </div>
             {props.users.map(u => <div key={u.id}>
                 <span>
