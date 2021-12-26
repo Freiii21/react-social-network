29c815ca91541b877de9e31444051e71387ee9b7
Freiii21 <mrfreiii@gmail.com>
2021-12-26 17:02:11 +0300

    #42 lesson_63

---
+++ src/api/api.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY" : "419c229b-43d4-438c-9dc1-973ee316752b"
+    }
+})
+
+export const usersAPI = {
+    getUsers (currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data);
+    }
+}
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -10,9 +10,9 @@ import {
     unfollowAC,
     UserType
 } from '../../redux/users-reducer';
-import axios from 'axios';
 import {Users} from './Users';
 import {Preloader} from '../common/Preloader/Preloader';
+import { usersAPI } from '../../api/api';
 
 export type MapStateToPropsType = {
     users:UserType[]
@@ -35,24 +35,19 @@ export type UsersPropsType = MapStateToPropsType & MapDispatchToPropsType;
 export class UsersAPIComponent extends React.Component<UsersPropsType> {
     componentDidMount() {
         this.props.setIsFetchingAC(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-            withCredentials: true
-        })
-            .then(response => {
+
+        usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
                 this.props.setIsFetchingAC(false);
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
+                this.props.setUsers(data.items);
+                this.props.setTotalUsersCount(data.totalCount);
             })
     }
     onPageChanged = (pageNumber:number) => {
         this.props.setCurrentPage(pageNumber);
         this.props.setIsFetchingAC(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
-            withCredentials: true
-        })
-            .then(response => {
+        usersAPI.getUsers(pageNumber, this.props.pageSize).then(data => {
                 this.props.setIsFetchingAC(false);
-                this.props.setUsers(response.data.items)
+                this.props.setUsers(data.items)
             })
     }
     render(){
