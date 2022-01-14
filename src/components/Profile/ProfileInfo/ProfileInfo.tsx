91ba0d16f15b06215bc4cc719bccff04c37b3f24
Freiii21 <mrfreiii@gmail.com>
2022-01-14 10:15:48 +0300

    #42 lesson_71

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,6 +3,7 @@ import s from './ProfileInfo.module.css'
 import {Preloader} from '../../common/Preloader/Preloader';
 import {ProfileType} from '../../../redux/profile-reducer';
 import defaultUserPhoto from '../../../assets/users/user1.jpg';
+import {ProfileStatus} from './ProfileStatus';
 
 type ProfileInfoPropsType = {
     profile:ProfileType | null
@@ -15,16 +16,16 @@ const ProfileInfo = (props:ProfileInfoPropsType) => {
 
     return (
         <div>
-            <div className={s.image}>
+            {/*<div className={s.image}>
                 <img
                     src="/images/Profile/ProfileInfo/image.jpg"
                     alt=""
                 />
-            </div>
+            </div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.small ? props.profile.photos.small : defaultUserPhoto}/><br/>
                 <span>{props.profile.fullName}</span><br/>
-                <span>status: {props.profile.aboutMe}</span>
+                <ProfileStatus status={"Hello my friends"}/>
             </div>
         </div>
     )
