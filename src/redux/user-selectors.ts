24b22cfc1cc2c66ac86cacfbdc1a919c488f6646
Freiii21 <mrfreiii@gmail.com>
2022-02-19 17:27:47 +0300

    #42 lesson_83

--- src/redux/user-selectors.ts
+++ src/redux/user-selectors.ts
@@ -1,8 +1,19 @@
+import { createSelector } from 'reselect';
 import {AppStateType} from './redux-store';
 
 export const getUsers = (state:AppStateType) => {
     return state.usersPage.users;
 }
+//Reselector Exapmple
+//Where getUsers is a simple selector and users is a result of getUsers and at the same moment a Dependency.
+//If the users has not change from the last run, then reselector will return the previous value.
+//For using our getUsersSuperSelector we have to call it like getUsersSuperSelector(state), because the simple
+//selector requires "state".
+//
+// const getUsersSuperSelector = createSelector(getUsers, (users) => {
+//     return users.filter(u=>true);
+// })
+
 export const getPageSize = (state:AppStateType) => {
     return state.usersPage.pageSize;
 }
