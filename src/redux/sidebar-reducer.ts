6280af872d558576dd44de02f975c2ba7e08759e
Freiii21 <mrfreiii@gmail.com>
2022-03-02 23:35:45 +0300

    #42 added new friend

--- src/redux/sidebar-reducer.ts
+++ src/redux/sidebar-reducer.ts
@@ -4,6 +4,7 @@ import Scarlett from './../assets/sidebarFriends/Scarlett.jpg'
 import Johnny from './../assets/sidebarFriends/Johnny.jpg'
 import Rachel from './../assets/sidebarFriends/Rachel.jpg'
 import Ross from './../assets/sidebarFriends/Ross.jpg'
+import Phoebe from './../assets/sidebarFriends/Phoebe.jpg'
 
 export type FriendType = {
     id: number
@@ -18,6 +19,7 @@ let initialState:InitialStateSidebarType = {
         {id: 3, name: 'Johnny', avatar: Johnny},
         {id: 4, name: 'Rachel', avatar: Rachel},
         {id: 5, name: 'Ross', avatar: Ross},
+        {id: 6, name: 'Phoebe', avatar: Phoebe},
     ]
 }
 export type InitialStateSidebarType = {
