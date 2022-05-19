bbb73ee13071da712f7c11a9c7e274d239625067
Freiii21 <mrfreiii@gmail.com>
2022-05-20 00:34:33 +0300

    #42 ui improvements 14

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import s from "./New.module.css"
+import news1 from "./../../assets/news/imageNews1.jpg"
 
 export const News = () => {
+    const news = [
+        {image:news1,area:"",title:"",news:""}
+    ]
+
+    // const newsList;
+
     return (
-        <div>
-            News
+        <div className={s.newsTab}>
+            <div className={s.newsTitle}>
+                <div>Daily news</div>
+            </div>
+            <div className={s.newsRows}>
+                <div className={s.singleNewsFiled}>
+                      <div className={s.newsTextFiled}>
+                          <div></div>
+                          <div></div>
+                          <div></div>
+                      </div>
+                    <img src={news1} alt="" className={s.newsImage}/>
+                </div>
+                <div className={s.singleNewsFiled}>
+                    <div className={s.newsTextFiled}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <img src={news1} alt="" className={s.newsImage}/>
+                </div>
+                <div className={s.singleNewsFiled}>
+                    <div className={s.newsTextFiled}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <img src={news1} alt="" className={s.newsImage}/>
+                </div>
+                <div className={s.singleNewsFiled}>
+                    <div className={s.newsTextFiled}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <img src={news1} alt="" className={s.newsImage}/>
+                </div>
+                <div className={s.singleNewsFiled}>
+                    <div className={s.newsTextFiled}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <img src={news1} alt="" className={s.newsImage}/>
+                </div>
+            </div>
         </div>
     )
 }
