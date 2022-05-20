5c34465b04510c1c30da87c55f5aa2e2c6e43ff8
Freiii21 <mrfreiii@gmail.com>
2022-05-20 14:43:04 +0300

    #42 ui improvements 15

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import s from "./New.module.css"
 import news1 from "./../../assets/news/imageNews1.jpg"
+import news2 from "./../../assets/news/imageNews2.jpg"
+import news3 from "./../../assets/news/imageNews3.jpg"
+import news4 from "./../../assets/news/imageNews4.jpg"
 
 export const News = () => {
     const news = [
-        {image:news1,area:"",title:"",news:""}
+        {id: 1, image:news1,title:"Taco Bell is bringing back the Mexican pizza - and South Asians are rejoicing",
+            news:"Taco Bell's Mexican Pizza was a staple comfort food for South Asians across the country. Now the" +
+                "fast food chain is bring the favorite back to its menu."},
+        {id: 2, image:news2,title:"Will Elon Musk turn activist at Twitter?",
+            news:"Musk is now Twitter's largest shareholder. That gives him a legal right to coerce and cajole" +
+                "and persuade and shout about what the company does."},
+        {id: 3, image:news3,title:"The French fry lover in your life may go head over peels for this perfume",
+            news:"Looking for a last-minute Valentine's Day gift? Idaho's potato commission is giving away French" +
+                "fry-scented perfume for Valentine's Day."},
+        {id: 4, image:news4,title:"A veteran won a $4 million lottery prize using the numbers from a fortune cookie",
+            news:"The North Carolina man turned his meal at an Asian restaurant into a Mega Millions jackpot when " +
+                "his cookies turned out to be extra fortunate."},
     ]
 
-    // const newsList;
+    const newsList = news.map(n =>  <div key={n.id} className={s.singleNewsFiled}>
+        <div className={s.newsTextFiled}>
+            <div className={s.singleNewsTitle}>{n.title}</div>
+            <div className={s.singleNewsText}>{n.news}</div>
+        </div>
+        <img src={n.image} alt="" className={s.newsImage}/>
+    </div>)
 
     return (
         <div className={s.newsTab}>
@@ -15,46 +35,7 @@ export const News = () => {
                 <div>Daily news</div>
             </div>
             <div className={s.newsRows}>
-                <div className={s.singleNewsFiled}>
-                      <div className={s.newsTextFiled}>
-                          <div></div>
-                          <div></div>
-                          <div></div>
-                      </div>
-                    <img src={news1} alt="" className={s.newsImage}/>
-                </div>
-                <div className={s.singleNewsFiled}>
-                    <div className={s.newsTextFiled}>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <img src={news1} alt="" className={s.newsImage}/>
-                </div>
-                <div className={s.singleNewsFiled}>
-                    <div className={s.newsTextFiled}>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <img src={news1} alt="" className={s.newsImage}/>
-                </div>
-                <div className={s.singleNewsFiled}>
-                    <div className={s.newsTextFiled}>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <img src={news1} alt="" className={s.newsImage}/>
-                </div>
-                <div className={s.singleNewsFiled}>
-                    <div className={s.newsTextFiled}>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <img src={news1} alt="" className={s.newsImage}/>
-                </div>
+                {newsList}
             </div>
         </div>
     )
