baddbf7892024bcca555dfaceb58097bcf4a8f74
Freiii21 <mrfreiii@gmail.com>
2022-05-24 23:38:03 +0300

    #42 ui improvements 24

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import s from './Paginator.module.css';
-
+import {ReactComponent as First} from './../../../assets/paginator/first.svg';
+import {ReactComponent as Last} from './../../../assets/paginator/last.svg';
+import {ReactComponent as Prev} from './../../../assets/paginator/prev.svg';
+import {ReactComponent as Next} from './../../../assets/paginator/next.svg';
+import {ReactComponent as PrevArray} from './../../../assets/paginator/prevArray.svg';
+import {ReactComponent as NextArray} from './../../../assets/paginator/nextArray.svg';
 
 type PaginatorPropsType = {
     totalItemssCount: number
@@ -30,14 +35,56 @@ export const Paginator = (props: PaginatorPropsType) => {
                                 onClick={(e) => props.onPageChanged(p)}
         >{p}</span>)
 
+    const onFirstPage = () => {
+        setPortionNumber(1);
+        props.onPageChanged(1);
+    }
+    const onLastPage = () => {
+        setPortionNumber(portionCount);
+        props.onPageChanged(pagesCount);
+    }
+    const onPreviousPage = () => {
+        props.onPageChanged(props.currentPage - 1)
+        if(props.currentPage - 1 < leftPortionPageNumber){
+            setPortionNumber(portionNumber - 1)
+        }
+    }
+    const onNextPage = () => {
+        props.onPageChanged(props.currentPage + 1);
+        if(props.currentPage+1 > rightPortionPageNumber){
+            setPortionNumber(portionNumber + 1)
+        }
+    }
+    const onPreviousArray = () => {
+        setPortionNumber(portionNumber - 1)
+        props.onPageChanged(rightPortionPageNumber - portionSize);
+    }
+    const onNextArray = () => {
+        setPortionNumber(portionNumber + 1)
+        props.onPageChanged(leftPortionPageNumber + portionSize);
+    }
+
     return (
         <div className={s.paginatorField}>
-            <button onClick={() => { setPortionNumber(portionNumber -1)}}
-                    disabled={portionNumber <= 1}
-                    className={s.button}>PREV</button>
+            <button onClick={onFirstPage} disabled={props.currentPage <= 1} className={s.button}>
+                <First/>
+            </button>
+            <button onClick={onPreviousArray} disabled={portionNumber <= 1} className={s.button}>
+                <PrevArray/>
+            </button>
+            <button onClick={onPreviousPage} disabled={props.currentPage <= 1} className={s.button}>
+                <Prev/>
+            </button>
             <div className={s.numbers}>{displayedPages}</div>
-            { portionCount > portionNumber &&
-            <button onClick={() => { setPortionNumber(portionNumber +1)}} className={s.button}>NEXT</button>}
+            <button onClick={onNextPage} disabled={props.currentPage >= pagesCount} className={s.button}>
+                <Next/>
+            </button>
+            <button onClick={onNextArray} disabled={portionNumber >= portionCount} className={s.button}>
+                <NextArray/>
+            </button>
+            <button onClick={onLastPage} disabled={props.currentPage >= pagesCount} className={s.button}>
+                <Last/>
+            </button>
         </div>
     )
 }
